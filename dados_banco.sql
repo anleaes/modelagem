05ea804b2ab9065994c076daccec133fa29835b8
anleaes <anleaes@gmail.com>
2025-06-06 11:45:35 -0300

    dsad

--- dados_banco.sql
+++ dados_banco.sql
@@ -1,3 +1,5 @@
+-- Inserindo dados ma tabela autor
+
 INSERT INTO autor (nome, idade) values ('Antonio Leaes', 36);
 INSERT INTO autor (nome, idade) values ('Antonio Leaes 1', 22);
 INSERT INTO autor (nome, idade) values ('Antonio Leaes 2', 22);
@@ -5,12 +7,14 @@ INSERT INTO autor (nome, idade) values ('Antonio Leaes 3', 22);
 INSERT INTO autor (nome, idade) values ('Antonio Leaes 4', 43);
 
 
+-- Inserindo dados ma tabela pessoa
 
 INSERT INTO pessoa (nome, cpf) values ('Antonio Leaes 1', 99999);
 INSERT INTO pessoa (nome, cpf) values ('Antonio Leaes 2', 88888);
 INSERT INTO pessoa (nome, cpf) values ('Antonio Leaes 3', 77777);
 INSERT INTO pessoa (nome, cpf) values ('Antonio Leaes 4', 66666);
 INSERT INTO pessoa (nome, cpf) values ('Antonio Leaes 5', 55555);
+
 
 -- Inserindo dados ma tabela livro
 
@@ -36,6 +40,5 @@ VALUES ('10/10/2023', '12/10/2023', '12/10/2025', 2, 5);
 
 INSERT INTO emprestimo (data_retirada, data_devolucao_prevista, data_devolucao_efetiva, id_pessoa, id_livro) 
 VALUES ('10/10/2023', '12/10/2023', '12/10/2025', 5, 5);
-
 
 
